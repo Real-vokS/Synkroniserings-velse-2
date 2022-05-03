8aa02460fba8bb0f009a201faf9281cd3fa6f831
Real-vokS <danielcskov@gmail.com>
2022-05-03 09:09:49 +0200

    Add project files.

---
+++ Synkroniserings øvelse 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Synkroniserings_øvelse_2
+{
+    class Program
+    {
+
+        static int count = 0;
+        static object printLock = new object();
+
+
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(Print);
+            var t2 = new Thread(Print);
+
+            t1.Start("*");
+            Thread.Sleep(10);
+            t2.Start("#");
+        }
+
+        static void Print(object print)
+        {
+            while (true)
+            {
+                lock (printLock)
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        count++;
+                        Console.Write(print);
+                        Thread.Sleep(10);
+                    }
+
+                    Console.WriteLine($" {count}");
+                }
+            }
+
+        }
+    }
+}
